0204143f121cdf14576ea9949774bdebdafcb988
에라 <jyh@babygo.kr>
2022-10-31 10:57:44 +0900

    feat: 컬렉션뷰 추가 및 outlet 연결

---
+++ OpenMarket/OpenMarket/Controller/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  OpenMarket - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak private var viewTypeSeguement: UISegmentedControl!
+    @IBOutlet weak private var plusButton: UIBarButtonItem!
+    @IBOutlet weak var pagesCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
