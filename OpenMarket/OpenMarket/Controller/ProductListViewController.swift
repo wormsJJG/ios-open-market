145d183f4e15d0539a839a645c81b30dc01bb655
wormsJJG <jjr66461837@gmail.com>
2022-11-10 12:06:40 +0900

    fix: 개행 및 코드 정리

--- OpenMarket/OpenMarket/Controller/ProductListViewController.swift
+++ OpenMarket/OpenMarket/Controller/ProductListViewController.swift
@@ -6,43 +6,19 @@
 
 import UIKit
 
-final class MainViewController: UIViewController, UIGestureRecognizerDelegate {
+final class ProductListViewController: UIViewController, UIGestureRecognizerDelegate {
     
     enum Section {
         case main
     }
-    
+    // MARK: - const var
+    private var productListPage: ProductListPage?
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Page>!
+    // MARK: - UI Component
     @IBOutlet weak private var viewTypeSegmentControl: UISegmentedControl!
     @IBOutlet weak private var moveToAddViewButton: UIBarButtonItem!
     @IBOutlet weak private var pageCollectionView: CustomCollectionView!
-
     private let swipeGesture = UISwipeGestureRecognizer()
-    private var productListPage: ProductListPage?
-    
-    private lazy var dataSource = UICollectionViewDiffableDataSource<Section, Page>(collectionView: pageCollectionView) {
-        pageCollectionView, indexPath, itemIdentifier in
-        
-        switch CustomCollectionView.ViewType(rawValue: self.viewTypeSegmentControl.selectedSegmentIndex) {
-        case .list:
-            guard let cell = pageCollectionView.dequeueReusableCell(withReuseIdentifier: CellIdentifier.listCellID, for: indexPath) as? ListPageCell else {
-                
-                return UICollectionViewListCell()
-            }
-            cell.configureCell(page: itemIdentifier)
-            
-            return cell
-        case .grid:
-            guard let cell = pageCollectionView.dequeueReusableCell(withReuseIdentifier: CellIdentifier.gridCellID, for: indexPath) as? GridPageCell else {
-                
-                return UICollectionViewCell()
-            }
-            cell.configure(page: itemIdentifier)
-            
-            return cell
-        case .none:
-            return UICollectionViewListCell()
-        }
-    }
     
     private lazy var activityIndicator: UIActivityIndicatorView = {
         let activityIndicator = UIActivityIndicatorView()
@@ -55,13 +31,19 @@ final class MainViewController: UIViewController, UIGestureRecognizerDelegate {
         
         return activityIndicator
     }()
-    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.pageCollectionView.reloadData()
+    }
+    // MARK: - configure
     private func configure() {
+        configureDataSource()
         pageCollectionView.changeLayout(type: .list)
         registerCell()
         addIndicator()
@@ -84,24 +66,7 @@ final class MainViewController: UIViewController, UIGestureRecognizerDelegate {
         swipeGesture.addTarget(self, action: #selector(didSwipe))
         self.view.addGestureRecognizer(swipeGesture)
     }
-    
-    @IBAction func didChangeSegmentControl(_ sender: UISegmentedControl) {
-        switch CustomCollectionView.ViewType(rawValue: sender.selectedSegmentIndex) {
-        case .list:
-            pageCollectionView.changeLayout(type: .list)
-            pageCollectionView.reloadData()
-        case .grid:
-            pageCollectionView.changeLayout(type: .grid)
-            pageCollectionView.reloadData()
-        case .none:
-            return
-        }
-    }
-    
-    private func changeLayoutAction() {
-        
-    }
-    
+    // MARK: - Data
     private func getProductListPage() {
         DispatchQueue.global().async {
             NetworkManager.getData(requestType: .productList(pageNo: 1, itemsPerPage: 100)) { result in
@@ -130,6 +95,54 @@ final class MainViewController: UIViewController, UIGestureRecognizerDelegate {
             snapShot.appendItems(pages)
             self?.dataSource.apply(snapShot, animatingDifferences: false)
             self?.activityIndicator.stopAnimating()
+        }
+    }
+    
+    private func configureDataSource() {
+        self.dataSource = UICollectionViewDiffableDataSource<Section, Page>(collectionView: pageCollectionView) {
+            pageCollectionView, indexPath, itemIdentifier in
+            
+            switch CustomCollectionView.ViewType(rawValue: self.viewTypeSegmentControl.selectedSegmentIndex) {
+            case .list:
+                guard let cell = pageCollectionView.dequeueReusableCell(withReuseIdentifier: CellIdentifier.listCellID, for: indexPath) as? ListPageCell else {
+                    
+                    return UICollectionViewListCell()
+                }
+                cell.configureCell(page: itemIdentifier)
+                
+                return cell
+            case .grid:
+                guard let cell = pageCollectionView.dequeueReusableCell(withReuseIdentifier: CellIdentifier.gridCellID, for: indexPath) as? GridPageCell else {
+                    
+                    return UICollectionViewCell()
+                }
+                cell.configure(page: itemIdentifier)
+                
+                return cell
+            case .none:
+                return UICollectionViewListCell()
+            }
+        }
+    }
+    // MARK: - Action
+    private func changeLayoutAction() {
+        pageCollectionView.reloadData()
+        activityIndicator.startAnimating()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) { [self] in
+            activityIndicator.stopAnimating()
+        }
+    }
+    
+    @IBAction func didChangeSegmentControl(_ sender: UISegmentedControl) {
+        switch CustomCollectionView.ViewType(rawValue: sender.selectedSegmentIndex) {
+        case .list:
+            pageCollectionView.changeLayout(type: .list)
+            changeLayoutAction()
+        case .grid:
+            pageCollectionView.changeLayout(type: .grid)
+            changeLayoutAction()
+        case .none:
+            return
         }
     }
     
